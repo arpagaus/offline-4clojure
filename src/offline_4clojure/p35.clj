2f63cbc64527a2360733a1efd8b31566b654321f
Remo Arpagaus <remo.arpagaus@bsi-software.com>
2016-09-02 17:56:20 +0200

    Solution for p35

--- src/offline_4clojure/p35.clj
+++ src/offline_4clojure/p35.clj
@@ -11,7 +11,7 @@
 
 (defn -main []
   (are [soln] soln
-(= __ (let [x 5] (+ 2 x)))
-(= __ (let [x 3, y 10] (- y x)))
-(= __ (let [x 21] (let [y 3] (/ x y))))
+(= 7 (let [x 5] (+ 2 x)))
+(= 7 (let [x 3, y 10] (- y x)))
+(= 7 (let [x 21] (let [y 3] (/ x y))))
 ))
