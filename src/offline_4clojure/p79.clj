61ce9407684aaf7b3299a15b62b2462bfa242ccf
Remo Arpagaus <arpagaus.remo@gmail.com>
2017-04-30 18:34:17 +0200

    Solution for p79

--- src/offline_4clojure/p79.clj
+++ src/offline_4clojure/p79.clj
@@ -6,19 +6,29 @@
   (:use clojure.test))
 
 (def __
-;; your solution here
-)
+  (fn min-path
+    ([tri] (min-path tri 0 0))
+    ([tri sum x] 
+      ;;(println "sum:" sum "x:" x)
+      (if (empty? tri)
+        sum
+        (let [v (-> tri first (get x))
+              s2 (+ sum v)
+              l (min-path (rest tri) s2 x)
+              r (min-path (rest tri) s2 (inc x))]
+          ;;(println "v:" v "s2:" "l:" l "r:" r)
+          (min l r))))))
 
 (defn -main []
   (are [soln] soln
-(= 7 (__ '([1]
-          [2 4]
-         [5 1 4]
-        [2 3 4 5]))) ; 1->2->1->3
-(= 20 (__ '([3]
-           [2 4]
-          [1 9 3]
-         [9 9 2 4]
-        [4 6 6 7 8]
-       [5 7 3 5 1 4]))) ; 3->4->3->2->7->1
+    (= 7 (__ '([1]
+              [2 4]
+             [5 1 4]
+            [2 3 4 5]))) ; 1->2->1->3
+    (= 20 (__ '([3]
+               [2 4]
+              [1 9 3]
+              [9 9 2 4]
+             [4 6 6 7 8]
+            [5 7 3 5 1 4]))) ; 3->4->3->2->7->1
 ))
